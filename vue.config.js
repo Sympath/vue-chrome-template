8a5c5b3f869b4ed6f381b6d38da6e022bccf21a6
Wzyan <3101885298@qq.com>
2022-10-10 15:17:04 +0800

    feat: 考虑到脚手架选择时会删除一些文件夹 对chrome配置项的处理

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,10 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
-
+const fs = require("fs");
 // Generate pages object
 const pagesObj = {};
-
-const chromeName = ["popup", "options", "content-script"];
-
-chromeName.forEach(name => {
-  pagesObj[name] = {
-    entry: `src/${name}/index.js`,
-    template: "public/index.html",
-    filename: `${name}.html`
-  };
-});
+let entry = {};
 let commonPlugins = [
-  { from: 'src/content-script/cs-init.js', to: 'content-script/cs-init.js' },
-  { from: 'src/content-script/content-script.css', to: 'content-script/content-script.css' },
   {
     from: 'src/manifest.json',
     to: 'manifest.json',
@@ -31,15 +20,40 @@ let commonPlugins = [
     },
   }
 ]
+const chromeOptions = ["override-page", "background", "popup", "options", "content-script"];
+// 考虑到脚手架选择时会删除一些文件夹 对chrome配置项的处理
+chromeOptions.forEach(name => {
+  let chromePages = ["override-page", "popup", "options", "content-script"]
+  let isExist = fs.existsSync(path.resolve(__dirname, `src/${name}`))
+  if (isExist) {
+    if (name === 'background') {
+      entry.background = "./src/background/main.js"
+    }
+    // 如果content-script存在 需要额外添加其他配置
+    if (name === 'content-script') {
+      commonPlugins.concat([
+        { from: 'src/content-script/cs-init.js', to: 'content-script/cs-init.js' },
+        { from: 'src/content-script/content-script.css', to: 'content-script/content-script.css' },
+      ])
+    }
+    if (chromePages.includes(name)) {
+      pagesObj[name] = {
+        entry: `src/${name}/index.js`,
+        template: "public/index.html",
+        filename: `${name}.html`
+      };
+    }
+
+  }
+});
+
 module.exports = {
   pages: pagesObj,
   configureWebpack: {
-    entry: {
-			background: "./src/background/main.js"
-		},
-		output: {
-			filename: "js/[name].js"
-		},
+    entry,
+    output: {
+      filename: "js/[name].js"
+    },
     plugins: [CopyWebpackPlugin(commonPlugins)]
   }
 };
